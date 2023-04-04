c8d5a59f09b9a1f458886d6366ef213b36001705
imsikun <imsikun16@gmail.com>
2023-04-04 23:05:44 +0530

    added elements in the navbar

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,5 +1,48 @@
+'use client';
+
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div
+      className="
+   border-[1px]
+   w-full
+   md:w-auto
+   rounded-full
+   shadow-sm
+   hover:shadow-md
+   trnasition
+   cursor-pointer
+
+  "
+    >
+      <div
+        className="
+        flex
+        flex-row
+        items-center
+        justify-between
+       "
+      >
+        <div
+          className="
+       text-sm
+       font-semibold
+       px-6
+
+      "
+        >
+          Anywhere
+        </div>
+        <div className="hidden sm:block text-sm font-semibold px-6 border-x- flex-1 text-center">
+          {' '}
+          Any Week
+        </div>
+        <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
+          Any Day
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
