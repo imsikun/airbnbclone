1e4fec9b9096abe60aa6fb9978dd4d5ce8e8ec79
imsikun <imsikun16@gmail.com>
2023-04-04 21:57:45 +0530

    first commit

---
+++ app/components/Container.tsx
@@ -0,0 +1,16 @@
+//ok so this line means it's a client component, as we are going to use this in other files as well.
+'use client';
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
+      {children}
+    </div>
+  );
+};
+
+export default Container;
